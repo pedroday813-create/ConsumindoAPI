322f58fbfa63217a9cc8edd0de515e5ca2e044b6
PEDRO RODRIGUES CRUZ <25203773@CFP127ALUNO.LOCAL>
2026-04-27 10:10:53 -0300

    Fazendo

---
+++ app.js
@@ -0,0 +1,23 @@
+'USE STRICT';
+
+ async function getDadosCep(cep) {
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const response = await fetch(url)
+    const data = await response.json()
+    return data;
+}
+
+async function preencherEndereco() {
+    const cep = document.getElementById('cep').value
+    const dadosCep = await getDadosCep(cep)
+
+    document.getElementById('endereco').value = dadosCep.logradouro
+    document.getElementById('bairro').value = dadosCep.bairro
+    document.getElementById('cidade').value = dadosCep.localidade
+    document.getElementById('estado').value = dadosCep.estado
+    document.getElementById('cep').value = dadosCep.cep
+
+
+}
+
+document.getElementById('cep').addEventListener('focusout', preencherEndereco);
